eb1f58b7a8efc43bcda33a99f387d294c6de35f1
Ernell Veraldi <ernellveraldi@gmail.com>
2023-12-20 21:56:32 -0600

    add functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,36 @@
 // Assignment code here
+function generatePassword() {
+
+  const passwordLength = prompt("How many characters for password [8 - 128]");
+
+  // check edge case for user closing prompt without entering information
+  if (passwordLength === "") return "Please enter a password length";
+  // check edge case for user entering password length <8 or >128
+  if (passwordLength < 8 || passwordLength > 128) return "Please enter a number between 8 and 128";
+  // check edge case for user entering characters
+  if (isNaN( Number(passwordLength))) return "Please enter only numbers";
+
+
+  const passwordLowercase = prompt("Use lowercase (Y/n)");
+
+  // do edge cases here
+
+  const passwordUppercase = prompt("Use uppercase (Y/n)");
+
+  // do edge cases here
+
+  const passwordNumeric = prompt("Use numeric (Y/n)");
+
+  // do edge cases here
+
+  const passwordSpecialCharacters  = prompt("Special Characters (Y/n)");
+
+  // do edge cases here
+
+
+
+
+}
 
 
 // Get references to the #generate element
